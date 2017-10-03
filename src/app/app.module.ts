9744fe99c6c87644ca7710bd8efbb48363a83fc6
Marcin Grabski <m.grabski89@hotmail.com>
2017-10-03 18:12:47 +0200

    Add custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent} from './products/product-list.component'
- 
+import { ConvertToSpaces } from './share/convert-to-spaces.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpaces
   ],
   imports: [
     BrowserModule
